0b92ee4c0861e9abaf5a8cd523934d5189a5f4ff
Donnie <xdonniehd@gmail.com>
2020-04-08 23:44:08 +0100

    First Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav');
-    const navLinks = document.querySelectorAll('.nav li');
+    const burger = document.querySelector('div.burger');
+    const nav = document.querySelector('ul.burger');
+    const navLinks = document.querySelectorAll('li.burger');
     
     //Toggle Nav
     burger.addEventListener('click',()=> {
